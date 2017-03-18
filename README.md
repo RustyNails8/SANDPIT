4099b864370b223580b8199773f6e0748ab69ab8
Sumit Das <sumit8das@gmail.com>
2017-03-19 05:04:54 +0530

    Modified gitignore-global

--- README.md
+++
@@ -1,3 +0,0 @@
-This is a readme file.
-I have to find out what md means.
-I added a line to check staging and working area difference.
--- gitcommands.sh
+++
@@ -1,4 +0,0 @@
-#git add file
-#git checkout master
-I have been added by the AnewBranch !
-I have been added by the AnewBranch for the second time !!
